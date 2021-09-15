e807cd829df620fd0ccf6aa4cc90673c9e907792
ishiguro-yo <4243145+yoh1496@users.noreply.github.com>
2021-09-15 16:13:04 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: ['@babel/polyfill', './src/app/frontend/index.js'],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build/public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: `${__dirname}/tools`,
+    publicPath: '/__/public/',
+    openPage: '__/front/app',
+    historyApiFallback: {
+      rewrites: [{ from: /^\/__\/front\/app/, to: '/dev_index.html' }],
+    },
+  },
+};
